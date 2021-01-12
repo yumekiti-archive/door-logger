7885b859f69de5ca1408a3c053a59b375f938677
kinoshita0623 <kuutarousii@gmail.com>
2021-01-12 16:22:23 +0900

    change Modelのリレーションを実装した

--- server/laravel/app/Models/Device.php
+++ server/laravel/app/Models/Device.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\DoorLog;
 
 class Device extends Model
 {
     use HasFactory;
+
+    public function logs()
+    {
+        return $this->hasMany(DoorLog::class);
+    }
 }
--- server/laravel/app/Models/DoorLog.php
+++ server/laravel/app/Models/DoorLog.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Device;
 
 class DoorLog extends Model
 {
     use HasFactory;
+
+    public function device()
+    {
+        return $this->belongsTo(Device::class);
+    }
 }
