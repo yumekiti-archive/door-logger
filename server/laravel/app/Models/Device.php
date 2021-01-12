da9a91ee97796548d044cbeff9e97543fe203073
kinoshita0623 <kuutarousii@gmail.com>
2021-01-13 01:14:25 +0900

    token生成などを実装した

--- server/laravel/app/Models/Device.php
+++ server/laravel/app/Models/Device.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\DoorLog;
 use App\Models\User;
+use Illuminate\Support\Str;
 
 class Device extends Model
 {
@@ -14,6 +15,11 @@ class Device extends Model
     public function logs()
     {
         return $this->hasMany(DoorLog::class);
+    }
+
+    public function latestLog()
+    {
+        return $this->logs()->whereRaw('logs.device_id = (select max(id) from logs where logs.device_id = devices.id)');
     }
 
     public function user()
@@ -31,6 +37,15 @@ class Device extends Model
     {
         $log = new DoorLog(['is_open' => false]);
         $this->logs()->save($log);
+    }
+
+    /**
+     * tokenを生成します。
+     */
+    public function generateToken()
+    {
+        $this->token = hash('sha-256', Str::random(10));
+        return $this->save();
     }
 
 }
