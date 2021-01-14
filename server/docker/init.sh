59f633603a24da53bedbb5f473e1b753f73b6834
yumekiti <yumekiti120440@gmail.com>
2021-01-14 09:15:22 +0900

    fix: sh間違えていたので変更

--- server/docker/init.sh
+++ server/docker/init.sh
@@ -1,8 +1,6 @@
 export user=$(id -u):$(id -g) && \
 docker-compose up --build -d && \
-cd ../laravel && \
-composer install && \
-cp .env.example .env && \
-php artisan key:generate && \
-php artisan migrate && \
-cd ../docker
+docker-compose exec php composer install && \
+docker-compose exec php cp .env.example .env && \
+docker-compose exec php php artisan key:generate && \
+docker-compose exec php php artisan migrate
