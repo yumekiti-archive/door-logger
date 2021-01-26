b0544eed8e4ac32af96852eda930e5a5c9175299
yumekiti <yumekiti120440@gmail.com>
2021-01-26 17:45:29 +0900

    feat: redis

--- server/laravel/app/Http/Controllers/DoorAPIController.php
+++ server/laravel/app/Http/Controllers/DoorAPIController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\DoorRequest;
 use App\Models\Device;
+use App\Events\DoorEvent;
 
 class DoorAPIController extends Controller
 {
@@ -16,13 +17,15 @@ class DoorAPIController extends Controller
     public function door(DoorRequest $request)
     {
         $device = Device::where('token', $request->bearerToken())->first();
+        $log;
         if($request->input('is_open')){
-            $device->openDoor();
+            $log = $device->openDoor();
         }else{
-            $device->closeDoor();
+            $log = $device->closeDoor();
         }
 
+        \Log::debug("hoge");
+        event(new DoorEvent($log));
         return $device;
     }
 }
-
--- server/laravel/routes/channels.php
+++ server/laravel/routes/channels.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use App\Models\DoorLog;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+/*
+Broadcast::channel('door-event-{id}', function($user, $id) {
+    return (int)$user->id === (int) $id;
+});
+*/
