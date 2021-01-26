092b7ed69c8295c3b2c7799d224ead46fce04882
yumekiti <yumekiti120440@gmail.com>
2021-01-26 18:23:35 +0900

    add 変更に基づいて特定のIdとClassのある要素の内容を書き換えるJSを作成&実装した

--- server/laravel/resources/js/door_state_updater.js
+++ server/laravel/resources/js/door_state_updater.js
@@ -1,1 +1,20 @@
 const echo = window.Echo;
+
+
+const updateState = (e) => {
+    let doorLog = e.doorLog;
+    let id = e.doorLog.id;
+    console.log(id);
+    let deviceElement = document.getElementById("device-id-" + doorLog.device_id);
+    console.assert(deviceElement != null, "device elementを取得することができませんでした");
+    let elements = deviceElement.getElementsByClassName("door-state");
+    if(elements.length){
+        let element = elements[0];
+        element.innerText = doorLog.is_open ? "OPEN" : "CLOSE";
+    }
+}
+echo.channel("doors.event")
+    .listen("DoorEvent", (e)=>{
+        console.log(e);
+        updateState(e);
+    });
