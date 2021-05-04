2a6427fdc0278a491f34bac1ea13a399635b3299
yumekiti <yumekiti120440@gmail.com>
2021-05-05 04:37:24 +0900

    add: npm

---
+++ raspberry-pi/sab/close.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import time
+import sys
+
+# 以下後に変更
+
+# トークン設定
+print("トークンを設定")
+token = "4c716d4cf211c7b7d2f3233c941771ad0507ea5bacf93b492766aa41ae9f720d"
+
+print("IPとポートを指定")
+ip = "localhost:8081"
+
+# 以下URLが等しくない時変更 URL設定
+url = "http://" + ip + "/api/device/door"
+
+# 変更内容ここまで
+
+# ヘッダーの設定
+headers = {"Authorization" : "Bearer "+ token, "Accept" : "application/json", 'Content-Type': 'application/json'}
+
+# ドアセンサーのon off設定
+door_sw = False
+
+# on off 確認
+print(door_sw)
+
+# パラメーター設定
+params = { "is_open": door_sw }
+
+# paramsをjsonに変更
+door_json = json.dumps(params)
+
+# サーバーへPOST
+res = requests.post(url, door_json, headers=headers)
+
+# 終わり確認
+print("end")
