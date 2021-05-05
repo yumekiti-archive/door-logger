a8aefffcffba3ef3e8ec8ef22be0296a87a486b1
yumekiti <yumekiti120440@gmail.com>
2021-05-05 21:10:32 +0900

    add: icon

---
+++ raspberry-pi/test.py
@@ -0,0 +1,68 @@
+import requests
+import RPi.GPIO as GPIO
+import json
+import time
+import sys
+
+# 以下後に変更
+
+# トークン設定
+print("トークンを設定")
+token = "4c716d4cf211c7b7d2f3233c941771ad0507ea5bacf93b492766aa41ae9f720d"
+
+print("IPとポートを指定")
+ip = "localhost:8081"
+
+# 以下URLが等しくない時変更 URL設定
+url = "https://" + ip + "/api/device/door"
+
+# 変更内容ここまで
+
+# ヘッダーの設定
+headers = {"Authorization" : "Bearer "+ token, "Accept" : "application/json", 'Content-Type': 'application/json'}
+
+# モードの指定をする(今回は役割ピン番号)
+GPIO.setmode(GPIO.BCM)
+# GPIO18pinを入力モードとし、pull up設定とします
+GPIO.setup(18,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+# ドアセンサーのon off設定
+door_sw = False
+
+# postループ対策
+sw_lock = False
+
+while True:
+    try:
+
+        # センサーの読み込み
+        door_sw = GPIO.input(18)
+
+        if door_sw != sw_lock:
+
+            # on off 確認
+            print(door_sw)
+
+            # パラメーター設定
+            params = { "is_open": door_sw }
+
+            # paramsをjsonに変更
+            door_json = json.dumps(params)
+
+            # サーバーへPOST
+            res = requests.post(url, door_json, headers=headers, verify = False)
+
+        sw_lock = door_sw
+
+        time.sleep(0.50)
+
+    except:
+        import traceback
+        traceback.print_exc()
+        break
+
+# GPIO操作終了
+GPIO.cleanup()
+
+# 終わり確認
+print("end")
