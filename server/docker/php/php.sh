7fde176cbe41f33be4e076ee5a112175e9777b45
yumekiti <yumekiti120440@gmail.com>
2021-05-10 13:38:46 +0900

    fix: docker

--- server/docker/php/php.sh
+++ server/docker/php/php.sh
@@ -2,7 +2,7 @@ flag=true
 
 while $flag; do
 
-  if (docker-compose logs | grep bind-address > /dev/null 2>&1) ; then
+  if (docker-compose -f ./docker/docker-compose.yml logs | grep bind-address > /dev/null 2>&1) ; then
     echo "Mysql Ready"
     flag=false
   fi
